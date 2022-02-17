bf8a18e794551ff436a948da57f27091fb07e01e
Charlie894 <63108760+Charlie894@users.noreply.github.com>
2022-02-17 21:07:02 +0800

    Update CNplus.py

--- CNplus.py
+++ CNplus.py
@@ -2,3 +2,17 @@ def 打印(a,b):
     str(b)
     if a==1:
         print(b)
+    elif a == 2:
+        print(b,end=' ')
+
+def 判断(a,b,c):
+    if b == '=':
+        if a == c:
+            return 'true'
+        else:
+            return 'false'
+    elif b == '!=':
+        if a != c:
+            return 'true'
+        else:
+            return 'false'
